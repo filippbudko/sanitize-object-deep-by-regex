4871b0749608c900d6583119cd0ddd0b423e5e5c
filippbudko <filipp.budko.dev@gmail.com>
2018-05-11 13:47:49 +0100

    ✨ Sanitise based on keys

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,4 +4,94 @@ describe('sanitise', () => {
   it('is a function', () => {
     expect(typeof sanitise).toBe('function');
   });
+
+  describe('when used without any opts', () => {
+    const obj = {
+      property1: 'value1',
+      property2: {
+        withEmail: 'hello@example.com',
+      },
+    };
+
+    it('returns object without changes', () => {
+      const sanitised = sanitise(obj);
+      expect(sanitised).toEqual({
+        property1: 'value1',
+        property2: {
+          withEmail: 'hello@example.com',
+        },
+      });
+    });
+  });
+
+  describe('when using keys option', () => {
+    const obj = {
+      property1: 'value1',
+      password: 'p4ssw0rd',
+      email: 'hello@example.com',
+    };
+    let sanitised;
+    beforeAll(() => {
+      sanitised = sanitise(obj, {
+        keys: ['password', 'email'],
+      });
+    });
+
+    it('replaces the values for those keys with [redacted]', () => {
+      expect(sanitised).toEqual({
+        property1: 'value1',
+        password: '[redacted]',
+        email: '[redacted]',
+      });
+    });
+
+    it('does not mutate the input obj', () => {
+      expect(sanitised).not.toEqual(obj);
+      expect(obj.password).toEqual('p4ssw0rd');
+    });
+  });
+
+  describe('when using deeply nested object', () => {
+    const obj = {
+      property1: 'value1',
+      password: 'p4ssw0rd',
+      email: 'hello@example.com',
+      deep: {
+        nested: {
+          array: [
+            {
+              email: 'hello@example.com',
+              password: 'p4ssw0rd',
+              values: ['value1'],
+            },
+          ],
+        },
+      },
+    };
+    let sanitised;
+    beforeAll(() => {
+      sanitised = sanitise(obj, {
+        keys: ['password', 'email'],
+      });
+    });
+
+    it('allows us to mask strings & keys keys', () => {
+      expect(sanitised).toEqual({
+        property1: 'value1',
+        password: '[redacted]',
+        email: '[redacted]',
+        deep: {
+          nested: {
+            array: [
+              {
+                password: '[redacted]',
+                email: '[redacted]',
+                values: ['value1'],
+              },
+            ],
+          },
+        },
+      });
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-export function sanitise() {
-  if (Math.random() === 1) {
-    return 1;
+export interface SanitiseOptions {
+  keys: string[];
+  replaceWith: string;
+}
+
+export function sanitise(obj, opts: Partial<SanitiseOptions> = {}): any {
+  const { replaceWith = '[redacted]', keys = [] } = opts;
+
+  if (Array.isArray(obj)) {
+    return obj.map(entry => sanitise(entry, opts));
   }
+  if (typeof obj === 'object') {
+    return Object.keys(obj).reduce((res, key) => {
+      const val = keys.includes(key) ? replaceWith : sanitise(obj[key], opts);
+
+      return Object.assign({}, res, { [key]: val });
+    }, {});
+  }
+
+  return obj;
 }
 
 export default sanitise;
